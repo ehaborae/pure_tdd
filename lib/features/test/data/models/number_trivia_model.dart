c59b037fc678b69e824df9b176a4321acc3e70e0
Ehab Borae Hassanien <68748080+ehaborae@users.noreply.github.com>
2022-08-09 15:40:34 +0200

    Data Model

--- lib/features/test/data/models/number_trivia_model.dart
+++ lib/features/test/data/models/number_trivia_model.dart
@@ -1,4 +1,4 @@
-import 'package:pure_tdd/features/test/domain/entities/number_trivia.dart';
+import '/features/test/domain/entities/number_trivia.dart';
 
 class NumberTriviaModel extends NumberTrivia {
   const NumberTriviaModel({required super.text, required super.number});
